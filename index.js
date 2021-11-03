4f04793cffc5b623679b4afa2cc2f855ce358168
Pacmard <030704@mail.ru>
2021-11-03 21:04:44 +0300

    wip

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const winston = require('winston');
 const semver = require('semver');
 
 if (semver.major(winston.version) !== 3) {
-    throw new Error(
-        "This module only supports winston v3"
-    );
+  throw new Error('This module only supports winston v3');
 }
